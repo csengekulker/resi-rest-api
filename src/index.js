4e65116643b7e810cd3f7d00718f3fe787ead995
csengekulker <k71608883429@ktch.hu>
2022-05-02 10:42:09 +0200

    adding employee in json format on buttonclick

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 const cl = console.log
 const URL = 'http://localhost:3000/employees'
+const button = document.getElementById("add")
 
+// fetch(URL)
+// .then(res => {
+//   cl(res)
+// })
+
+function getEmployeeData() {
 // request all data: promise
 fetch(URL)
 .then(res => res.json())
 .then(res => {
   cl(res)
 })
+}
+
+function addEmployee() {
+  const data = {
+    id: 7,
+    fullName: "Va Laki",
+    city: "Budapest",
+    salary: 3000,
+  }
+  
+  fetch(URL, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+    })
+  .then(res => cl(res))
+}
+
+getEmployeeData()
+
+button.addEventListener('click', addEmployee)
